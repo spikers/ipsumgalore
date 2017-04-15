f3343aa6adbadbaa10fb7d9472e3031135911ffa
spikers <timfengineering@gmail.com>
2017-04-15 16:25:27 -0700

    Complete: Allow custom start on first paragraph.

--- development/public/js/script.js
+++ development/public/js/script.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
 function initialize() {
   document.getElementById('generate-ipsum').addEventListener('click', function () {
     clearOutput();
-    appendIpsumToDom(hp, 5);
+    appendIpsumToDom(lotr, 5);
   });
 }
 
@@ -20,44 +20,52 @@ function appendIpsumToDom(arrayPool, paragraphs, words) {
 }
 
 function getIpsumDocumentFragment(arrayPool, paragraphs, words) {
+  words = checkWords(words);
+
   var ipsumElements = document.createDocumentFragment();
 
-  for (var i = 0; i < paragraphs; i++) {
-    var generatedIpsum = generateIpsum(arrayPool, paragraphs, words);
+  var firstParagraphTextNode = getFirstParagraphTextNode(arrayPool, words);
+  ipsumElements.appendChild(firstParagraphTextNode);
 
-    var pElement = document.createElement('p');
-    var tn = document.createTextNode(generatedIpsum);
+  for (var i = 1; i < paragraphs; i++) {
+    var generatedIpsum = '';
+    generatedIpsum += generateIpsum(arrayPool, words);
+    generatedIpsum = capitalizeFirstWord(generatedIpsum);
 
-    pElement.appendChild(tn);
-
+    var pElement = getIpsumParagraph(generatedIpsum);
+    
     ipsumElements.appendChild(pElement);
   }
 
   return ipsumElements;
 }
 
-function generateIpsum(arrayPool, paragraphs, words) {
-  if (words == undefined) words = 100;
+function getIpsumParagraph(content) {
+  var pElement = document.createElement('p');
+  var tn = document.createTextNode(content);
+  pElement.appendChild(tn);
+  return pElement;
+}
+
+function generateIpsum(arrayPool, words) {
+  words = checkWords(words);
   var ipsum = '';
 
   for (let j = 0; j < words; j++) {
     var poolIndex = Math.floor(Math.random() * arrayPool.length)
     var poolWord = arrayPool[poolIndex];
 
-    //First word of a paragraph is capitalized
-    if (j == 0) poolWord = capitalizeWord(poolWord);
-
     ipsum += poolWord;
 
     //This is so the end of paragraphs don't have an innocuous space
     if (j < words - 1) ipsum += ' ';
-
+    else ipsum += '.'
   }
 
   return ipsum;
 }
 
-function capitalizeWord(word) {
+function capitalizeFirstWord(word) {
   return word[0].toUpperCase() + word.substring(1);
 }
 
@@ -70,4 +78,23 @@ function appendToParagraphElement(stringToAppend) {
   pElement.appendChild(tn);
   output.appendChild(pElement);
   return;
+}
+
+function getFirstParagraphTextNode(arrayPool, words) {
+  var start = '';
+  if (true) {
+    start = 'Lorem ipsum dolor ';
+  }
+
+  //Generate the first paragraph
+  var firstParagraph = start 
+  ? start + generateIpsum(arrayPool, words - 3) 
+  : capitalizeFirstWord(generateIpsum(arrayPool, words));
+
+  return document.createTextNode(firstParagraph);
+}
+
+function checkWords(words) {
+  if (words == undefined) return 100;
+  else return words;
 }
